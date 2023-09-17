93c6c5ace632a06d6739ac4af1a5c88d6d6b9130
chaeryeon823 <chaechae823@naver.com>
2023-09-17 20:45:45 +0900

    Feat: colors 추가
    color 상수로 저장해두고, styled-components 라이브러리 설치.

---
+++ like-knu/src/constants/colors.js
@@ -0,0 +1,18 @@
+const colors = { 
+  common: "#A68968",
+  cheonAn: "#2C8E9A",
+  sinGwan: "#E85239",
+  yeSan: "#E2981E",
+
+  gray100: "#E2E2E2",
+  gray200: "#D1D3D2",
+  gray300: "#BDBDBD",
+  gray350: "#AFAFAF",
+  gray400: "#8A8A8A",
+
+  white: "#FFFFFF",
+  black: "#403E3F",
+  shadow: "rgba(0, 0, 0, 0.05)"
+
+
+}
