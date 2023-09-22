8e913dd1a9afc2e0b11c1a213ec6072910d4b864
chaeryeon823 <chaechae823@naver.com>
2023-09-22 19:52:34 +0900

    Feat: 메인 버스 데이터 불러오기

---
+++ like-knu/src/components/BusItem.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import colors from "constants/colors"
+
+export default function BusItem() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  background-color: purple;
+`
--- like-knu/src/components/main/MainBus.jsx
+++ like-knu/src/components/main/MainBus.jsx
@@ -1,11 +1,28 @@
 import styled from "styled-components"
 import CardContainer from "components/CardContainer"
 import colors from "constants/colors"
+import { bus } from "api/main"
+import { useState, useEffect } from "react"
 
 export default function MainBus() {
+  const [buses, setBuses] = useState([]);
+
+
+  const getBuses = async() => {
+    const res = await bus();
+    setBuses(res);
+  }
+  useEffect( () => {
+    getBuses();
+  },[]);
   return (
     <BusContainer>
       <Title>버스</Title>
+      {
+        buses.map((bus) => (
+          <div key={bus.routeId} bus={bus}>{bus.busNumber}</div>
+        ))
+      }
     </BusContainer>
   )
 }
