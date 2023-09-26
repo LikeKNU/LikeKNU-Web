c2a53644baee21f7b5dc5a630d1e87a9e755e5bc
chaeryeon823 <chaechae823@naver.com>
2023-09-26 21:33:09 +0900

    Feat: 헤더 아이콘 및 날짜 추가

--- like-knu/src/components/main/MainHeader.jsx
+++ like-knu/src/components/main/MainHeader.jsx
@@ -5,6 +5,8 @@ import DropDown from "components/DropDown"
 import { useEffect, useState } from "react"
 import {getCampus} from "utils/DeviceManageUtil"
 import {ReactComponent as DownIcon} from "assets/icon/expand_more_black_24dp.svg"
+import {ReactComponent as NotificationIcon} from "assets/icon/bell-fill.svg"
+import {ReactComponent as SettingIcon} from "assets/icon/gear-fill.svg"
 
 export default function MainHeader() {
   const [view, setview] = useState(false);
@@ -14,15 +16,26 @@ export default function MainHeader() {
     setCampusName(campus);
   }
 
+  const goNotification = () =>{
+    console.log("푸시알림 이동!!");
+  }
+
+  const goSetting = () => {
+    console.log("설정 이동!!");
+  }
 
   return (
     <Wrapper>
       <CampusList onClick={() => {setview(!view)}}>
         <Title $campus={campusName}>{campusName}</Title>
         {view && <DropDown changeCampus={changeCampus}/>}
+        <DownIcon fill={colors.black} />
       </CampusList>
-      <DownIcon fill={colors.black} />
-
+      <DateText>12월 31일 금요일</DateText>
+      <IconList>
+        <StyledNotification onClick={goNotification}/>
+        <StyledSetting onClick={goSetting}/>
+      </IconList>
     </Wrapper>
   )
 }
@@ -32,6 +45,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
   height: 6.4rem;
   margin-bottom: 16px;
   padding: 0px 16px;
@@ -66,4 +80,22 @@ const Title = styled.div`
         `
     }
   }}
+`
+const DateText = styled.div`
+  color: ${colors.gray400};
+  font-size: 1.4rem;
+  font-weight: 600;
+`
+const IconList = styled.div`
+`
+const StyledNotification = styled(NotificationIcon)`
+  width: 24px;
+  height: 24px;
+  color: #FFCB74;
+  margin-right: 14px;
+`
+const StyledSetting = styled(SettingIcon)`
+  width: 24px;
+  height: 24px;
+  color: #7F7C7D;
 `
