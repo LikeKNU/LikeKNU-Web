a9cda8026a711b51a98eef632b2f18fec37863a3
chaeryeon823 <chaechae823@naver.com>
2023-10-03 23:33:27 +0900

    Feat: 공지사항 메인으로 이동 버튼 추가

--- like-knu/src/layouts/PageContainer.js
+++ like-knu/src/layouts/PageContainer.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export default styled.div`
-  padding: 0px 16px;
+  padding: 0 16px;
   display: grid;
 `
--- like-knu/src/pages/NoticePage.jsx
+++ like-knu/src/pages/NoticePage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import PageLayout from "layouts/PageLayout";
+import PageContainer from "layouts/PageContainer";
+import BackHeader from "components/BackHeader";
 export default function NoticePage() {
   return (
-    <div>NoticePage</div>
+    <PageLayout>
+      <BackHeader pageName={"공지사항"}/>
+      <PageContainer>
+      </PageContainer>
+    </PageLayout>
   )
 }
