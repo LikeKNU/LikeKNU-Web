a63e5c9bba413587be54270804a473b45171b6ae
jcw1031 <jcw001031@gmail.com>
2023-10-18 14:03:45 +0900

    Fix: 서비스 워커

--- like-knu/src/index.js
+++ like-knu/src/index.js
@@ -22,7 +22,13 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
+if (navigator.serviceWorker) {
+    // Register the SW
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+        .then(function(registration){
+    }).catch(console.log);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
