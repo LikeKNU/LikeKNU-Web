266999279bc2d5e5abed5d9bb9fedc2ce5d2c611
chaeryeon823 <chaechae823@naver.com>
2023-11-17 22:26:26 +0900

    feat: 시내버스 목적지 버튼 추가

--- like-knu/src/components/bus/CityBus.jsx
+++ like-knu/src/components/bus/CityBus.jsx
@@ -1,10 +1,30 @@
 import styled from "styled-components";
+import { BusDestination } from "./BusDestination";
+import { useState } from "react";
+import PageContainer from "../../layouts/PageContainer";
+const school = ["학교로 출발", "학교로 도착"];
 
 function CityBus() {
-  return <Wrapper>안녕</Wrapper>;
+  const [destination, setDestination] = useState(0);
+  return (
+    <Wrapper>
+      <BusDestinationArea>
+        {school.map((text, index) => (
+          <BusDestination
+            key={index}
+            onClick={() => setDestination(index)}
+            className={destination === index ? "active" : null}
+          >
+            {text}
+          </BusDestination>
+        ))}
+      </BusDestinationArea>
+    </Wrapper>
+  );
 }
-const Wrapper = styled.div`
-  margin-top: 140px;
+const Wrapper = styled(PageContainer)``;
+const BusDestinationArea = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
-
 export default CityBus;
