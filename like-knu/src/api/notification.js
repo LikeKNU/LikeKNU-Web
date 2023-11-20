de031c9f61b9ef01246e858b00738092629014c9
jcw1031 <jcw001031@gmail.com>
2023-11-20 21:08:55 +0900

    fix: 알림 페이지

--- like-knu/src/api/notification.js
+++ like-knu/src/api/notification.js
@@ -4,11 +4,11 @@ import {getDeviceId} from "../utils/DeviceManageUtil";
 const endpointPrefix = "/api/notifications";
 
 export const fetchNotifications = async (page) => {
-  const response = await instance.get(`${endpointPrefix}`, {
+  const { data } = await instance.get(`${endpointPrefix}`, {
     params: {
       deviceId: getDeviceId(),
       page: page
     }
   });
-  return response.data;
+  return data.data;
 }
