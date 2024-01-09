ae7be328992124406cd58817c71e1eba5f8aaac4
jcw1031 <jcw001031@gmail.com>
2024-01-09 16:34:22 +0900

    조정

--- like-knu/src/KakaoAdFit.jsx
+++ like-knu/src/KakaoAdFit.jsx
@@ -16,7 +16,7 @@ function KakaoAdFit({ unit, width, height, disabled }) {
     }
   }, [])
 
-  return <div ref={scriptElementWrapper} style={{ alignSelf: 'center', marginTop: 74 }}>
+  return <div ref={scriptElementWrapper} style={{ alignSelf: 'center', marginTop: 62 }}>
     <ins className="kakao_ad_area" style={{ display: 'none' }}
          data-ad-unit={unit}
          data-ad-width={width}
