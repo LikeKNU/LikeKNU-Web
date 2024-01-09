6d4b71ff865a6077a3cd1b481f245d42d9349e18
jcw1031 <jcw001031@gmail.com>
2024-01-09 16:14:08 +0900

    Kakao AdFit 테스트

--- like-knu/src/pages/MainPage.jsx
+++ like-knu/src/pages/MainPage.jsx
@@ -1,14 +1,15 @@
-import MainNotice from "components/main/MainNotice";
-import React, { useState } from "react";
-import PageLayout from "layouts/PageLayout";
-import PageContainer from "layouts/PageContainer";
-import MainHeader from "components/main/MainHeader";
-import MainBus from "components/main/MainBus";
-import MainMenu from "components/main/MainMenu";
-import MainCalendar from "components/main/MainCalendar";
-import styled from "styled-components";
-import colors from "constants/colors";
-import { getCampus } from "../utils/DeviceManageUtil";
+import MainBus from 'components/main/MainBus';
+import MainCalendar from 'components/main/MainCalendar';
+import MainHeader from 'components/main/MainHeader';
+import MainMenu from 'components/main/MainMenu';
+import MainNotice from 'components/main/MainNotice';
+import colors from 'constants/colors';
+import PageContainer from 'layouts/PageContainer';
+import PageLayout from 'layouts/PageLayout';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import KakaoAdFit from '../KakaoAdFit';
+import { getCampus } from '../utils/DeviceManageUtil';
 
 export default function MainPage() {
   const [selectCampus, setSelectCampus] = useState(getCampus);
@@ -17,6 +18,7 @@ export default function MainPage() {
     <PageLayout>
       <MainHeader setSelectCampus={setSelectCampus} />
       <StyledPageContainer>
+        <KakaoAdFit unit={'DAN-fVYT1aWQnMOribRe'} width={'320'} height={'50'} disabled={false} />
         <MainNotice selectCampus={selectCampus} />
         <MainBus selectCampus={selectCampus} />
         <MainMenu selectCampus={selectCampus} />
@@ -27,11 +29,11 @@ export default function MainPage() {
 }
 
 const StyledPageContainer = styled(PageContainer)`
-  background-color: ${colors.GRAY50};
-  padding-top: 74px;
-  grid-template-rows: minmax(100px, auto);
-  grid-template-columns: 1fr 1fr;
+    background-color: ${colors.GRAY50};
+    padding-top: 74px;
+    grid-template-rows: minmax(100px, auto);
+    grid-template-columns: 1fr 1fr;
 
-  grid-column-gap: 14px;
-  grid-row-gap: 12px;
+    grid-column-gap: 14px;
+    grid-row-gap: 12px;
 `;
