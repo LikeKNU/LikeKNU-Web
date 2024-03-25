c034eaf6ee550e12a39568de6e9e3b1f0e3d36b3
jcw1031 <jcw001031@gmail.com>
2024-03-25 10:07:16 +0900

    hotfix: PWA install onboarding page

--- like-knu/src/App.js
+++ like-knu/src/App.js
@@ -63,7 +63,7 @@ function App() {
   // PWA로 설치되지 않은 상태
   const isAndroid = /Android/.test(navigator.userAgent);
   const isiOS = /(iPhone|iPad|iPod)/.test(navigator.userAgent);
-  /*if (isiOS && !window.matchMedia('(display-mode: standalone)').matches) {
+  if (isiOS && !window.matchMedia('(display-mode: standalone)').matches) {
     return (
       <>
         <Image src={IosImage} alt={'뭘봐'} />
@@ -75,7 +75,7 @@ function App() {
         <Image src={OtherImage} alt={'뭘봐'} />
       </>
     );
-  }*/
+  }
   initializeDevice();
   RouteChangeTracker();
   initializeDeviceColor();
